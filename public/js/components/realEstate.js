5c2e1cfc6bfba3d20204a511abc42ed129662981
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-28 13:31:21 -0800

    Created App Component

--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -24,96 +24,41 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Layout = function (_Component) {
-  _inherits(Layout, _Component);
+var App = function (_Component) {
+  _inherits(App, _Component);
 
-  function Layout() {
-    _classCallCheck(this, Layout);
+  function App() {
+    _classCallCheck(this, App);
 
-    var _this = _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).call(this));
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
 
     _this.clickedBtn = function () {
       console.log('swag');
     };
 
     _this.state = {
-      name: 'Joe'
+      name: 'Louis'
     };
     return _this;
   }
 
-  _createClass(Layout, [{
+  _createClass(App, [{
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
         'div',
-        { className: 'home' },
-        _react2.default.createElement(
-          'div',
-          { className: 'Aligner' },
-          _react2.default.createElement(
-            'div',
-            { className: 'Aligner-item' },
-            _react2.default.createElement('img', { src: '/img/logo.png' }),
-            _react2.default.createElement(
-              'h1',
-              null,
-              'Starter-Kit-2k18'
-            ),
-            _react2.default.createElement(
-              'div',
-              { className: 'menu' },
-              _react2.default.createElement(
-                'ul',
-                null,
-                _react2.default.createElement(
-                  'div',
-                  { onClick: this.clickedBtn },
-                  'clickked this'
-                ),
-                _react2.default.createElement(
-                  'li',
-                  null,
-                  _react2.default.createElement(
-                    'a',
-                    { href: 'http://starterkit.codingphase.com', target: 'new' },
-                    'Documentation'
-                  )
-                ),
-                _react2.default.createElement(
-                  'li',
-                  null,
-                  _react2.default.createElement(
-                    'a',
-                    { href: 'http://www.codingphase.com', target: 'new' },
-                    'CodingPhase.Com'
-                  )
-                )
-              )
-            ),
-            _react2.default.createElement(
-              'div',
-              { className: 'version-num' },
-              'version 2.0.18'
-            ),
-            _react2.default.createElement('br', null),
-            _react2.default.createElement(
-              'a',
-              { className: 'github-button', href: 'https://github.com/codingphasedotcom/Starter-Kit-2018', 'data-icon': 'octicon-star', 'data-style': 'mega', 'data-count-href': '/codingphasedotcom/rocky/stargazers', 'data-count-api': '/repos/codingphasedotcom/rocky#stargazers_count', 'data-count-aria-label': '# stargazers on GitHub', 'aria-label': 'Star codingphasedotcom/rocky on GitHub' },
-              'Star'
-            )
-          )
-        )
+        null,
+        'Real Estate'
       );
     }
   }]);
 
-  return Layout;
+  return App;
 }(_react.Component);
 
 var app = document.getElementById('app');
 
-_reactDom2.default.render(_react2.default.createElement(Layout, null), app);
+_reactDom2.default.render(_react2.default.createElement(App, null), app);
 
 /***/ })
 
