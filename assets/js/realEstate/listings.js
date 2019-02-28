de031ef9ebbfc2f75a3a5cb10826ae2f5e7f4b8d
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-28 14:12:49 -0800

    Added Listing Compnonent

--- assets/js/realEstate/listings.js
+++ assets/js/realEstate/listings.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react'
 
-export default class Header extends Component {
+export default class Listing extends Component {
   constructor () {
     super()
     this.state = {
@@ -9,16 +9,9 @@ export default class Header extends Component {
   }
   render () {
     return (
-      <header>
-        <div className="logo">Logo</div>
-
-        <nav>
-          <a href="#">Create Ads</a>
-          <a href="#">About Us</a>
-          <a href="#">Log In</a>
-          <a href="#" className="register btn">Register</a>
-        </nav>
-      </header>
+      <div>
+        
+      </div>
     )
   }
 }
